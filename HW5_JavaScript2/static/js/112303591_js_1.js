6e6baa1a2e3afe92c953bd59075c65ff080351f1
miku0219 <iml940219@gmail.com>
2025-10-02 22:26:35 +0800

    doc:add time events

--- HW5_JavaScript2/static/js/112303591_js_1.js
+++ HW5_JavaScript2/static/js/112303591_js_1.js
@@ -1,23 +1,46 @@
 let GNum = Math.floor(Math.random() * 101);
+let startTime;
+let timerInterval;
+let Gaming = false;
+
+const start = document.getElementById("GB");
+
 N = 0;
+
+function startG() {
+  if (!Gaming) {
+    startTime = new Date().getTime();
+    timerInterval = setInterval(updateTimer, 10);
+    Gaming = true;
+  }
+}
+
+function updateTimer() {
+  const timerElement = document.getElementById("getTime");
+  const currentTime = new Date().getTime();
+  const elapsedTime = Math.floor((currentTime - startTime) / 10) / 100;
+  timerElement.textContent = elapsedTime;
+}
 function Guess() {
   const num = parseInt(document.getElementById("guess").value);
-  const result = document.getElementById("guessresult");
-  const oldresult = result.querySelector("p");
-  const newresult = document.createElement("p");
+  const result = document.getElementById("guessResult");
+
   if (num > GNum) {
-    newresult.textContent = "猜0~100之間的整數：太大了，請再試一次。";
-    result.replaceChild(newresult, oldresult);
+    result.textContent = "猜0~100之間的整數：太大了，請再試一次。";
     N++;
   } else if (num < GNum) {
-    newresult.textContent = "猜0~100之間的整數：太小了，請再試一次。";
-    result.replaceChild(newresult, oldresult);
+    result.textContent = "猜0~100之間的整數：太小了，請再試一次。";
     N++;
   } else if (num === GNum) {
+    clearInterval(timerInterval);
+    const endTime = new Date().getTime();
+    const totalTime = Math.floor((endTime - startTime) / 10) / 100;
     N++;
-    newresult.textContent = "猜0~100之間的整數：猜中了！共猜了" + N + "次";
-    result.replaceChild(newresult, oldresult);
-    GNum = Math.floor(Math.random() * 100);
+    result.textContent =
+      "猜0~100之間的整數：猜中了！共猜了" + N + "次，花了" + totalTime + "秒";
+    alert("猜中了！共猜了" + N + "次，花了" + totalTime + "秒");
+    GNum = Math.floor(Math.random() * 101);
     N = 0;
+    Gaming = false;
   }
 }
