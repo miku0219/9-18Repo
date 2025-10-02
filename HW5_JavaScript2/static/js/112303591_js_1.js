0b286a98e279f0fb91fd4ffe590f85b7e056bbab
miku0219 <iml940219@gmail.com>
2025-10-03 01:08:47 +0800

    doc:add list & fix mistakes

--- HW5_JavaScript2/static/js/112303591_js_1.js
+++ HW5_JavaScript2/static/js/112303591_js_1.js
@@ -3,9 +3,8 @@ let startTime;
 let timerInterval;
 let Gaming = false;
 
-const start = document.getElementById("GB");
-
 N = 0;
+A = 0;
 
 function startG() {
   if (!Gaming) {
@@ -37,10 +36,31 @@ function Guess() {
     const totalTime = Math.floor((endTime - startTime) / 10) / 100;
     N++;
     result.textContent =
-      "猜0~100之間的整數：猜中了！共猜了" + N + "次，花了" + totalTime + "秒";
+      "猜0~100之間的整數：猜中了！共猜了" + N + "次，花了" + totalTime + " 秒";
     alert("猜中了！共猜了" + N + "次，花了" + totalTime + "秒");
+    A++;
+    const GList = document.getElementById("guessList");
+    const NList = document.createElement("p");
+    const now = new Date();
+    const time = now.toLocaleTimeString("zh-TW", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+    NList.textContent =
+      A + ". 猜了" + N + "次，耗時" + totalTime + " 秒 " + time;
+    GList.appendChild(NList);
     GNum = Math.floor(Math.random() * 101);
     N = 0;
     Gaming = false;
   }
 }
+
+// function List() {
+//   const GList = document.getElementById("guessList");
+//   const NList = document.createElement("li");
+//   const now = new Date();
+//   NList.textContent = "猜了" + N + "次，耗時" + totalTime + " 秒 " + now;
+//   GList.appendChild();
+// }
